b5ccf962a9fb9a76c1d1a5a035c7a2694f4ed1ca
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-01-17 17:08:06 -0300

    Fetching deep data

--- index.js
+++ index.js
@@ -6,53 +6,77 @@ const cheerio = require('cheerio');
 const args = process.argv.slice(2);
 
 async function crawl(urlData) {
-  const { body } = await got.get(urlData);
-  const $ = cheerio.load(body);
-
   let data = [];
 
-  for (let i = 0; i < $('a').length; i += 1) {
-    let anchor = $('a').eq(i);
-    let href = anchor.attr('href');
-    let isNotUndefined = href !== undefined;
-    let isNotEmpty = isNotUndefined && href.trim() !== '';
-    let isNotHash = isNotUndefined && !(href.trim().indexOf('#') === 0);
-    let hasNotHrefInData = isNotUndefined && data.indexOf(href.trim()) === -1;
-    let hasOneFirstSlash = isNotUndefined && (href.trim().indexOf('/') > -1 && href.trim().indexOf('//') === -1);
-    let hasDoubleFirstSlash = isNotUndefined && (href.trim().indexOf('//') === 0);
+  try {
+    const { body } = await got.get(urlData);
+    const $ = cheerio.load(body);
 
-    if (hasOneFirstSlash) {
-      href = url.resolve(urlData, href);
-    } if (hasDoubleFirstSlash) {
-      href = 'http:' + href;
-    } if (isNotEmpty && (isNotHash && hasNotHrefInData)) {
-      data.push(href);
+
+    for (let i = 0; i < $('a').length; i += 1) {
+      let anchor = $('a').eq(i);
+      let href = anchor.attr('href');
+      let isNotUndefined = href !== undefined;
+      let isNotEmpty = isNotUndefined && href.trim() !== '';
+      let isNotHash = isNotUndefined && !(href.trim().indexOf('#') === 0);
+      let hasNotHrefInData = isNotUndefined && data.indexOf(href.trim()) === -1;
+      let hasOneFirstSlash = isNotUndefined && (href.trim().indexOf('/') > -1 && href.trim().indexOf('//') === -1);
+      let hasDoubleFirstSlash = isNotUndefined && (href.trim().indexOf('//') === 0);
+
+      if (hasOneFirstSlash) {
+        href = url.resolve(urlData, href);
+      } if (hasDoubleFirstSlash) {
+        href = 'http:' + href;
+      } if (isNotEmpty && (isNotHash && hasNotHrefInData)) {
+        data.push(href);
+      }
     }
+  } catch (e) {
+    return data;
   }
 
   return data;
 }
 
-(async function () {
-  let data = await crawl(args[0]);
+let dataCache = [];
 
-  let isDeepCrawler = args[1] !== undefined && args[1] === 'enabled';
+async function leveledCrawler(item) {
+  try {
+    let subData = await crawl(item);
 
-  for (let item of data) {
-    console.log(item);
+    for (let subItem of subData) {
+      if (dataCache.indexOf(subItem) === -1) {
+        dataCache.push(subItem);
+        console.log(subItem);
+      }
 
-    if (isDeepCrawler) {
-      try {
-        let subData = await crawl(item);
+      run(subItem);
+    }
+  } catch (e) {
+    return;
+  }
+}
 
-        for (let subItem of subData) {
-          if (data.indexOf(subItem) === -1) {
-            console.log('--', subItem);
-          }
-        }
-      } catch (e) {
-        break;
+async function run(entrypoint) {
+  try {
+    let data = await crawl(entrypoint);
+    let isDeepCrawler = args[1] !== undefined && args[1] === 'enabled';
+
+    for (let item of data) {
+      if (dataCache.indexOf(item) === -1) {
+        dataCache.push(item);
+        console.log(item);
+      }
+
+      if (isDeepCrawler) {
+        leveledCrawler(item);
       }
     }
+  } catch(e) {
+    return;
   }
+}
+
+(async function () {
+  run(args[0]);
 })();
