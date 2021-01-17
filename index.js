ea6980e2431d8b9392dd4fae2716b40526edb018
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-01-17 15:08:42 -0300

    Working on the recursive call of the crawler

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cheerio = require('cheerio');
 
 const args = process.argv.slice(2);
 
-(async function () {
-  const { body } = await got.get(args[0]);
+async function crawl(urlData) {
+  const { body } = await got.get(urlData);
   const $ = cheerio.load(body);
 
   let data = [];
@@ -18,8 +18,41 @@ const args = process.argv.slice(2);
     let isNotEmpty = isNotUndefined && href.trim() !== '';
     let isNotHash = isNotUndefined && !(href.trim().indexOf('#') === 0);
     let hasNotHrefInData = isNotUndefined && data.indexOf(href.trim()) === -1;
-    if (isNotEmpty && (isNotHash && hasNotHrefInData)) {
-      console.log(href);
+    let hasOneFirstSlash = isNotUndefined && (href.trim().indexOf('/') > -1 && href.trim().indexOf('//') === -1);
+    let hasDoubleFirstSlash = isNotUndefined && (href.trim().indexOf('//') === 0);
+
+    if (hasOneFirstSlash) {
+      href = url.resolve(urlData, href);
+    } if (hasDoubleFirstSlash) {
+      href = 'http:' + href;
+    } if (isNotEmpty && (isNotHash && hasNotHrefInData)) {
+      data.push(href);
+    }
+  }
+
+  return data;
+}
+
+(async function () {
+  let data = await crawl(args[0]);
+
+  let isDeepCrawler = args[1] !== undefined && args[1] === 'enabled';
+
+  for (let item of data) {
+    console.log(item);
+
+    if (isDeepCrawler) {
+      try {
+        let subData = await crawl(item);
+
+        for (let subItem of subData) {
+          if (data.indexOf(subItem) === -1) {
+            console.log('--', subItem);
+          }
+        }
+      } catch (e) {
+        break;
+      }
     }
   }
 })();
